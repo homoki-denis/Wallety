6675695acebb2b3e99675ac7d83ab27a98e6c347
Denis Homoki <homoki.denisth@gmail.com>
2022-11-05 14:35:58 +0200

    added form submitted

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,20 @@
 import silhouette from "./assets/silhouette.png";
+import { useState } from "react";
 
 export default function App() {
+  // States
+  const [value, setValue] = useState("");
+
+  // Functions
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(`You just submit ${value} avatars`);
+  };
+
+  const handleChange = (e) => {
+    setValue(e.target.value);
+  };
+
   return (
     <div className="px-2.5">
       <div className="space-y-8 bg-white flex justify-center items-center flex-col shadow-2xl main-border py-20 px-32 ">
@@ -16,8 +30,13 @@ export default function App() {
             <input
               type="number"
               className="rounded-full w-6/12 text-center my-3 text-xl font-bold input-border p-2.5"
+              value={value}
+              onChange={handleChange}
             />
-            <button className="btn rounded-full font-bold py-4 px-16 my-5 text-2xl">
+            <button
+              onClick={handleSubmit}
+              className="btn rounded-full font-bold py-4 px-16 my-5 text-2xl"
+            >
               MINT NOW
             </button>
           </form>
